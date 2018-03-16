5688ff58cfb0715edd7a92b1514f911d98c060d4
Max <lfmax223@gmail.com>
2018-03-16 11:51:35 -0500

    Add source maps for webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+    devtool: 'source-map',
     entry: "./src/js/app.js",
     output: {
         path: path.join(__dirname, 'public/js/'),
