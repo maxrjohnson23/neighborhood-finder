624da535a12e58b0fb4a57e77663d2b13535c0b3
Max <lfmax223@gmail.com>
2018-03-16 11:44:59 -0500

    File cleanup and implement webpack

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+// Essentials
+require('./essential/main');
+
+// Layout
+require('./layout/main');
+
+// Sidebar
+require('./sidebar/main');
+
+// Owl Carousel
+require('./media/carousel/main');
+
+// Maps
+window.initGoogleMaps = require('./maps/google/main');
+require('./maps/_maps');
+
+require('./main.js');
