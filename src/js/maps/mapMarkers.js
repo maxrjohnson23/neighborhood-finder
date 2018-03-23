bff1c8749c62b2791496105b2240498088081b6c
Max Johnson <maxrjohnson23@gmail.com>
2018-03-22 19:46:16 -0500

    toggle filter

--- src/js/maps/mapMarkers.js
+++ src/js/maps/mapMarkers.js
@@ -6,12 +6,22 @@ module.exports = function () {
         type: "GET",
         success: function (data) {
             console.log(data);
+            mapControls.activeHobbies = new Set();
+            mapControls.activeSocial = new Set();
 
             // Map each marker by lat/long
             data.forEach((a) => {
+                // Store active hobbies and social on the map filter
+                let hobbies = a.Hobbies.map(h => {
+                    mapControls.activeHobbies.add(h.id);
+                    return h.id;
+                });
+                let social = a.Socials.map(s => {
+                    mapControls.activeSocial.add(s.id);
+                    return s.id;
+                });
+
                 // Store hobby and social array on each marker
-                let hobbies = a.Hobbies.map(h => h.id);
-                let social = a.Socials.map(s => s.id);
                 let latLng = new google.maps.LatLng(a.geocodeLat, a.geocodeLng);
                 let marker = new google.maps.Marker({
                     map: map,
@@ -20,7 +30,8 @@ module.exports = function () {
                     social: social,
                     icon: "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png"
                 });
-                // Store data points in mapControls for global access
+
+                // Store data points in mapControls for map filtering
                 mapControls.mapMarkers.push(marker);
                 mapControls.mapLatLng.push(latLng);
 
--- src/js/maps/mapOverlay.js
+++ src/js/maps/mapOverlay.js
@@ -1,6 +1,8 @@
+// var clustering = require('density-clustering');
+
 module.exports = function () {
     // Register global map controls
-    mapControls = {
+    global.mapControls = {
         neighborHoodLayer: null,
         heatMap: null,
         mapLatLng: [],
@@ -8,16 +10,34 @@ module.exports = function () {
     };
 
     function toggleNeighborhoodOverlay(map) {
-        if (window.mapControls.neighborHoodLayer === null || window.mapControls.neighborHoodLayer.getMap() === null) {
+        if (global.mapControls.neighborHoodLayer === null || global.mapControls.neighborHoodLayer.getMap() === null) {
             let neighborhoodLayer = new google.maps.KmlLayer({
                 url: 'http://chicagomap.zolk.com/sources/neighborhoods/source.kml',
                 preserveViewport: true,
                 map: map
             });
-            window.mapControls.neighborHoodLayer = neighborhoodLayer;
+            global.mapControls.neighborHoodLayer = neighborhoodLayer;
         } else {
-            window.mapControls.neighborHoodLayer.setMap(null);
+            global.mapControls.neighborHoodLayer.setMap(null);
         }
+    }
+
+    function updateMapMarkers() {
+        mapControls.mapMarkers.forEach(m => {
+            let shouldDisplay = false;
+
+            for (let hobby of m.hobbies) {
+                if (mapControls.activeHobbies.has(hobby)) {
+                    shouldDisplay = true;
+                }
+            }
+            for (let social of m.social) {
+                if (mapControls.activeSocial.has(social)) {
+                    shouldDisplay = true;
+                }
+            }
+            m.setMap((shouldDisplay) ? map : null);
+        });
     }
 
     function createHeatMap() {
@@ -30,27 +50,30 @@ module.exports = function () {
     }
 
     function updateHeatMap() {
-        mapControls.mapLatLng = mapControls.mapMarkers.filter(m => {
-            return m.getMap();
-        }).map(m => {
-            return m.position;
-        });
-        mapControls.heatMap.setData(mapControls.mapLatLng);
+        if (mapControls.heatMap && mapControls.heatMap.getMap()) {
+
+            mapControls.mapLatLng = mapControls.mapMarkers.filter(m => {
+                return m.getMap();
+            }).map(m => {
+                return m.position;
+            });
+            mapControls.heatMap.setData(mapControls.mapLatLng);
+        }
     }
 
 
     // Register overlay toggle
     $("#neighborhood-map").on('click', function () {
         $(this).toggleClass("active");
-        toggleNeighborhoodOverlay(window.map);
+        toggleNeighborhoodOverlay(global.map);
     });
 
     // Register heatmap toggle
     $("#heat-map").on('click', function () {
-        if(!mapControls.heatMap) {
+        if (!mapControls.heatMap) {
             createHeatMap();
         }
-        if($(this).hasClass("active")){
+        if ($(this).hasClass("active")) {
             mapControls.heatMap.setMap(null);
         } else {
             mapControls.heatMap.setData(mapControls.mapLatLng);
@@ -61,35 +84,132 @@ module.exports = function () {
 
     });
 
+    $("#hobbies-all-toggle").on("click", function () {
+        const $el = $(this);
+
+        const $allHobbies = $(".sidebar-toggle[data-category='hobbies']");
+
+        if ($el.hasClass("active")) {
+            $allHobbies.removeClass("active");
+            mapControls.activeHobbies.clear();
+        } else {
+            $allHobbies.addClass("active");
+            $allHobbies.each(function () {
+                let hobbyId = $(this).data("filter-id");
+                mapControls.activeSocial.add(hobbyId);
+            });
+        }
+        updateMapMarkers();
+        updateHeatMap();
+        $el.toggleClass("active");
+    });
+
+    $("#social-all-toggle").on("click", function () {
+        const $el = $(this);
+
+        const $allSocial = $(".sidebar-toggle[data-category='social']");
+
+        if ($el.hasClass("active")) {
+            $allSocial.removeClass("active");
+            mapControls.activeSocial.clear();
+        } else {
+            $allSocial.addClass("active");
+            $allSocial.each(function () {
+                let socialId = $(this).data("filter-id");
+                mapControls.activeSocial.add(socialId);
+            });
+        }
+        updateMapMarkers();
+        updateHeatMap();
+        $el.toggleClass("active");
+    });
+
     // Register marker filters
     $(".sidebar-toggle").on('click', function () {
 
+        const $el = $(this);
         // Get category and id to filter the map markers
-        let filterId = $(this).data("filter-id");
-        let category = $(this).data("category");
+        const isDelete = $el.hasClass("active");
+        const filterId = $el.data("filter-id");
+        const category = $el.data("category");
 
-        if ($(this).hasClass("active")) {
-            mapControls.mapMarkers.forEach(m => {
-
-                // Remove markers from map
-                if (m[category].includes(filterId)) {
-                    m.setMap(null);
-                }
-            });
-        } else {
-            mapControls.mapMarkers.forEach(m => {
-                if (m[category].includes(filterId)) {
-                    m.setMap(map);
-                }
-            });
+        if (category === "hobbies") {
+            if (isDelete) {
+                mapControls.activeHobbies.delete(filterId);
+            } else {
+                mapControls.activeHobbies.add(filterId);
+            }
+        }
+        if (category === "social") {
+            if (isDelete) {
+                mapControls.activeSocial.delete(filterId);
+            } else {
+                mapControls.activeSocial.add(filterId);
+            }
         }
 
-        // If heatmap overlay is on
-        if(mapControls.heatMap && mapControls.heatMap.getMap()) {
-            updateHeatMap();
-        }
+        // Refresh map overlays
+        updateMapMarkers();
+        updateHeatMap();
+
 
         $(this).toggleClass("active");
     });
+
+    // function centerPoint(markers) {
+    //     let newmarkers = markers.filter(m => {
+    //         return m.getMap();
+    //     }).map(m => {
+    //         return m.position;
+    //     });
+    //     console.log(newmarkers);
+    //     var dataset = [];
+    //
+    //     for (var i = 0; i < newmarkers.length; i++) {
+    //         dataset.push([newmarkers[i].lat(), newmarkers[i].lng()]);
+    //     }
+    //
+    //     var dbscan = new clustering.DBSCAN();
+    //     var clusters = dbscan.run(dataset, 1, 500);
+    //
+    //     if (clusters.length > 0) {
+    //
+    //         /* Find the biggest cluster */
+    //         var biggestCluster = clusters[0];
+    //
+    //         for (i = 1; i < clusters.length; i++) {
+    //
+    //             if (cluster[i].length > biggestCluster.length) {
+    //                 biggestCluster = cluster[i];
+    //             }
+    //         }
+    //
+    //         /* The output of the library contains the indexes of the points in the cluster, not the coordinates, so we need to get the point from the dataset */
+    //         var clusterPoints = [];
+    //         var sumx = 0;
+    //         var sumy = 0;
+    //
+    //         for (i = 0; i < biggestCluster.length; i++) {
+    //             var point = dataset[biggestCluster[i]];
+    //             clusterPoints.push(point);
+    //
+    //             /* It's also a good opportunity to cumulate x and y so we can get the average */
+    //             sumx += point[0];
+    //             sumy += point[1];
+    //         }
+    //
+    //         console.log('Cluster:', clusterPoints);
+    //         console.log('Center:', sumx / clusterPoints.length, sumy / clusterPoints.length);
+    //
+    //         let ll = new google.maps.LatLng(sumx / clusterPoints.length, sumy / clusterPoints.length);
+    //         let marker = new google.maps.Marker({
+    //             map: map,
+    //             position: ll
+    //         });
+    //     }
+    //
+    // }
+    //
+    // global.center = centerPoint;
 
 };
