b1d56b4cc52965693b1cbdfa865864dbb4a5868f
Max <lfmax223@gmail.com>
2018-03-23 14:11:51 -0500

    remove passport

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var app = express();
 var passport = require('passport');
 var session = require('express-session');
 var routes = require("./controllers/controller.js");
-// var authRoutes = require('./controllers/authController');
-// var authRed = require('./authRoutes/auth')(app);
 var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
@@ -22,23 +20,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 // parse application/json
 app.use(bodyParser.json());
 
-// For Passport
- 
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
- 
-app.use(passport.initialize());
- 
-app.use(passport.session()); // persistent login sessions
-
-//load passport strategies
-require('./config/passport/passport')(passport, db.user);
-
-//For Handlebars
-app.set('views', './public')
-app.engine('hbs', exphbs({
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs');
 
 app.use(routes);
 
